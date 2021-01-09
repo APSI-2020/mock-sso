f22985ee22fb67de2fb9409433a7d9d9e7623ac7
Piotr Proszowski <p.proszowski@gmail.com>
2021-01-09 14:53:26 +0100

    Add template spring app

---
+++ src/main/java/pl/apsi/mocksso/MockSsoApplication.java
@@ -0,0 +1,13 @@
+package pl.apsi.mocksso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockSsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/apsi/mocksso/MockSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.apsi.mocksso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
